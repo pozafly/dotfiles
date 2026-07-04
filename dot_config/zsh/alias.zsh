913e6e4399a89346b1f622c1fde61b76e343704e
pozafly <pozafly@gmail.com>
2026-07-04 11:31:02 +0900

    feat: zsh에 brew update alias 추가 및 tmux 페이지 단위 스크롤 막기

--- dot_config/zsh/alias.zsh
+++ dot_config/zsh/alias.zsh
@@ -3,3 +3,4 @@ alias c='claude'
 alias cc='claude --dangerously-skip-permissions'
 alias cr='claude -r' # 최근대화 목록
 alias cs='claude -c' # 이전 대화 이어가기
+alias brewup='brew update && brew upgrade --yes'
